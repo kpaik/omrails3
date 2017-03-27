c67ab9589209bae44bcd0b259e0db8b7b79d22de
kay mins <katpaikh@gmail.com>
2017-03-26 19:56:14 -0700

    Change order of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
- root 'pages#home'
-  get 'about' => 'pages#about'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
-
-
-  devise_for :users
+ devise_for :users
   as :user  do
   	get "signin" => 'devise/sessions#new'
   	get "signup" => 'devise/registrations#new'
   	delete "signout" => 'devise/registrations#destroy'
   end
 
+  root 'pages#home'
+  get 'about' => 'pages#about'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
+
   
+
   
