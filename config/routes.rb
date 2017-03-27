bb5f7e98b115b4b665bb2a7a10833a19f316d1f4
kay mins <katpaikh@gmail.com>
2017-03-26 18:59:41 -0700

    Fixing Signout Page typo issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
-  as :user  do
-  	get "signin" => 'devise/sessions#new'
-  	delete "signout" => 'devise/registrations#destory'
-  	get "signup" => 'devise/registrations#new'
-  end
-
-  root 'pages#home'
+ root 'pages#home'
   get 'about' => 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+  devise_for :users
+  as :user  do
+  	get "signin" => 'devise/sessions#new'
+  	get "signup" => 'devise/registrations#new'
+  	delete "signout" => 'devise/registrations#destroy'
+  end
+
+  
