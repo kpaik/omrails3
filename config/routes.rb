330a52037fe72221e4d6f9bbe377d42758be908f
kay mins <katpaikh@gmail.com>
2017-03-26 17:52:38 -0700

    Added Sign in and out links and pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+  as :user  do
+  	get "signin" => 'devise/sessions#new'
+  	delete "signout" => 'devise/registrations#destory'
+  	get "signup" => 'devise/registrations#ew'
+  end
+
   root 'pages#home'
   get 'about' => 'pages#about'
-
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
